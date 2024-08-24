c40a9a24e6099a28b353a707cfeec9e0f321fb7e
Lava519 <lavaboi14@gmail.com>
2024-08-24 11:43:58 +0200

    Updated Search

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ function App() {
     data.results.forEach((item) => {
       pl.push(item.name);
     })
-    console.log(pl.length > 0)
     setPokemonList(pl);
   }
   useEffect(()=>{
@@ -29,20 +28,20 @@ function App() {
     <div className='flex h-screen items-center justify-center'>
       {loading ? 
       <Loading active={pokemonList > 0} trigger={()=>{setLoading(false)}}></Loading> : 
-      stateSwitch(state, setState)
+      stateSwitch(state, setState, pokemonList)
       }
     </div>
     </>
   )
 }
 
-function stateSwitch(state, setState) {
+function stateSwitch(state, setState, pokemonList) {
   switch (state) {
     case "Home":
       return (<Home state={setState}></Home>);
       break;
     case "Search":
-      return (<Search state={setState}></Search>);
+      return (<Search state={setState} pokeList={pokemonList}></Search>);
       break;
     case "Pokemon":
       return (<Pokemon state={setState}></Pokemon>);
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,47 @@
 import { useState } from 'react'
 
-function Search({state}) {
+function Search({state, pokeList}) {
   const [currAnimation, setCurrAnimation] = useState("animate-[pop-in_0.5s_forwards]")
+  const [autocomplete, setAutocomplete] = useState([]);
+  function handleSearchChange(e) {
+    e.preventDefault();
+    let k = [];
+    if (e.target.value.length > 1) {
+      let pattern = new RegExp(`^${e.target.value}`)
+      for (let i = 0; i < pokeList.length; ++ i) {
+        if (k.length > 4)
+          break;
+        if (pattern.test(pokeList[i])) {
+          k.push({id: k.length+1, name: pokeList[i]});
+        }
+      }
+      console.log(k);
+
+    }
+    setAutocomplete(k);
+  }
   function handleSubmit(e){
     e.preventDefault();
     setCurrAnimation("animate-[pop-out_0.5s_forwards]");
+    console.log(pokeList);
     setTimeout(() => {
       state("Pokemon");
     }, 500);
   }
   return (
-    <form onSubmit={handleSubmit} className={`flex ${currAnimation}`}>
-        <input className='text-center text-poke-black outline-none p-4 pr-[65px] text-2xl bg-poke-white rounded-3xl min-w-80' /><button action="submit" className='ml-[-60px] rounded-xl p-2 bg-poke-red flex justify-cemter items-center'><img src="./search.svg"></img></button>
+    <div>
+    <form onSubmit={handleSubmit} className={`relative flex ${currAnimation}`}>
+        <input onChange={handleSearchChange} className='text-center text-poke-black outline-none p-4 pr-[65px] text-2xl bg-poke-white rounded-3xl min-w-80' /><button action="submit" className='ml-[-60px] rounded-xl p-2 bg-poke-red flex justify-cemter items-center'><img src="./search.svg"></img></button>
+        <div className='absolute top-full flex flex-col items-center w-full gap-6'>
+          {autocomplete.map((pokemon)=> {
+            return (
+              <a className="text-3xl" key={pokemon.id}>{pokemon.name}</a>
+            )
+          })}
+        </div>
     </form>
+    </div>
+
   )
 }
 
