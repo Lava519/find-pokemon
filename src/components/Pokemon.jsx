0eaa8fb07c55198492b46bbcb0c1c11891cde906
Lava519 <lavaboi14@gmail.com>
2024-09-11 04:27:19 +0200

    Updated

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -29,7 +29,7 @@ function Pokemon({pokemon, state}) {
       {
         loaded &&
         <div className={`flex flex-col gap-10 ${currAnimation} lg:flex-row sc-h:mt-[150px]`}>
-          <div className='flex basis-3/5 bg-poke-gray p-4 min-w-[450px] rounded-2xl'>
+          <div className='flex basis-3/5 bg-poke-gray p-4 min-w-[350px] rounded-2xl'>
             <div className='bg-poke-black rounded-2xl'>
               <img src={pokemonData.sprites.other["official-artwork"].front_default}></img>
             </div>
